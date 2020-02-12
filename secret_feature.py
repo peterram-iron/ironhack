ef86735224afd6cce70103af93c95ede707344a6
peterram-iron <>
2020-02-12 00:00:16 +0000

    this is gonna be some feature

---
+++ secret_feature.py
@@ -0,0 +1,9 @@
+# this is one crazy feature, if it works we'll sell our company for millions
+
+if investors:
+	print("My startup uses AI")
+elif team:
+	print("guys, we need to develop some AI")
+
+else:
+	print("we have no money to pay salaries")
